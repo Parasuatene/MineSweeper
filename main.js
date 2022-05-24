bd3ab82c080950bd7e71ec061ae23a58ad36a234
Parasuatene <andoooo9.y.1127@gmail.com>
2022-05-24 16:50:45 +0900

    [add]周囲の地雷の個数を計算する処理を追加

--- main.js
+++ main.js
@@ -20,6 +20,29 @@ const createRandNum = () => {
     return arr.slice(0, 10);
 };
 
+// TODO: 後ほどリファクタリングを行う
+const calcMineCount = (index) => {
+    const addCount = (idx) => {
+        // 加算先が地雷でなければ＋1する
+        if (mineInfoArray[idx] != "M") { mineInfoArray[idx] += 1; } 
+    };
+
+    let leftCond = (index % 9 != 0);
+    let rightCond = ((index + 1) % 9 != 0);
+    let upCond = (index - 9 >= 0);
+    let downCond = (index + 9 < mineInfoArray.length);
+
+    // 加算処理
+    if (leftCond) { addCount(index-1) }  // 左方向
+    if (rightCond) { addCount(index+1) }  // 右方向
+    if (upCond) { addCount(index-9) }  // 上方向
+    if (downCond) { addCount(index+9) }  // 下方向
+    if (leftCond && upCond) { addCount(index-10); }  // 左上方向
+    if (leftCond && downCond) { addCount(index+8); }  // 左下方向
+    if (rightCond && upCond) { addCount(index-8); }  // 右上方向
+    if (rightCond && downCond) { addCount(index+10); }  // 右下方向
+};
+
 const setMines = () => {
     let randArr = createRandNum();
     randArr.forEach(i => {
